a17559fac3cd1bc33d003fab53e14810bcd522c4
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2014-05-24 19:19:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reviews
+
+Reviews is a child theme of Simone built as a demo of how to add custom post type and taxonomy data to a theme. There are several branches to this theme, each of which shows a different level of integration. The Master branch is the simplest application.
+
+Reviews requires a custom post type plugin with a post type named "review" and taxonomies named "genre", "rating", "feature", "mood", and "release_year".
---
+++ review-info.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template that displays movie info in a box
+ * Kicks in on single movie reviews and index pages
+ */
+?>
+
+<div class="movie-info clear">
+    <?php
+    // Do we have a featured image? If so, display it
+    if (has_post_thumbnail()){
+        echo '<figure class="movie-poster">';
+        the_post_thumbnail('poster-single');
+        echo '</figure>';
+    }
+    ?>
+    <div class="movie-data">
+        <div class="movie-tax">
+            <?php echo get_the_term_list( $post->ID, 'genre', '<h4 class="movie-data-title">Genre</h4>', ', ', '' ); ?> 
+        </div>
+        <div class="movie-tax">
+            <?php echo get_the_term_list( $post->ID, 'feature', '<h4 class="movie-data-title">Features</h4>', ', ', '' ); ?> 
+        </div>
+        <div class="movie-tax">
+            <?php echo get_the_term_list( $post->ID, 'mood', '<h4 class="movie-data-title">Moods</h4>', ', ', '' ); ?> 
+        </div>
+        <div class="movie-tax">
+            <?php echo get_the_term_list( $post->ID, 'rating', '<h4 class="movie-data-title">Rating</h4>', ', ', '' ); ?> 
+        </div>
+    </div>
+    <div class="short-review">
+        <h4 class="movie-data-title">Quick Review</h4>
+        <?php the_excerpt(); ?>
+    </div>
+</div><!-- .movie-info -->
+
+            
